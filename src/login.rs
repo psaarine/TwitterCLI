666cd3f421ced88629c59eef4addad4a8f228a8a
psaarine <saarinenps@gmail.com>
2022-03-27 17:02:17 +0300

    refactoring of login functionality

---
+++ src/login.rs
@@ -0,0 +1,28 @@
+fn login() {
+
+    /*
+
+    Opens a tcp connection that listens to a specific port.
+    Then directs user to a twitter login page that redirects to that particular tcp port.
+    Parses Id from the identification.
+
+     */
+}
+
+fn open_browser(url: &str) {
+
+    /*
+
+    Opens a browser in a specified url.
+
+     */
+}
+
+fn create_tcp_listener(){
+
+    /*
+
+    Creates a tcplistener to a specified port to capture redirectionUrl:s
+
+     */
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod login;
+
 fn main() {
 
 }
@@ -30,30 +32,3 @@ fn authenticate() -> AuthenticationResult {
     return AuthenticationResult {}
 }
 
-fn open_browser(url: &str) {
-
-    /*
-
-    Opens a browser in a specified url.
-
-     */
-}
-
-fn create_tcp_listener(){
-
-    /*
-
-    Creates a tcplistener to a specified port to capture redirectionUrl:s
-
-     */
-}
-
-fn login() {
-
-    /*
-
-    Opens a tcp connection that listens to a specific port.
-    Then directs user to a twitter login page that redirects to that particular tcp port.
-    Parses Id from the identification.
-     */
-}
