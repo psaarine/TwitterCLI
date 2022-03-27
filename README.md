5e2fa41b19e862abef270eb42c6f38e2d441d98b
psaarine <saarinenps@gmail.com>
2022-03-27 16:45:16 +0300

    raw modeling of functionality

---
+++ README.md
@@ -0,0 +1,6 @@
+# Twitter CLI
+
+This is a CLI that I am developing in order to understand authentication protocols
+of Twitter (and possibly other big tech). It is written in rust, since I dont want 
+to use components that are "ready" and refined. Instead I want to understand all of the
+functionality of an authentication library myself.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+
+pub struct AuthenticationResult {
+
+}
+
+fn basic_program_flow(handler: fn()){
+
+    /*
+
+    Basic program flow goes as follows:
+
+    Tries to execute handler. If receives unauthenticated, will first check expiration token validity.
+    If unable to get a valid token
+
+     */
+
+
+
+}
+fn authenticate() -> AuthenticationResult {
+
+    /*
+
+    Initializes an authentication flow. Opens a browser in the twitter login page and once user has signed in will capture redirection
+    and parse tokens from the
+     */
+    return AuthenticationResult {}
+}
+
+fn open_browser(url: &str) {
+
+    /*
+
+    Opens a browser in a specified url.
+
+     */
+}
+
+fn create_tcp_listener(){
+
+    /*
+
+    Creates a tcplistener to a specified port to capture redirectionUrl:s
+
+     */
+}
+
+fn login() {
+
+    /*
+
+    Opens a tcp connection that listens to a specific port.
+    Then directs user to a twitter login page that redirects to that particular tcp port.
+    Parses Id from the identification.
+     */
 }
