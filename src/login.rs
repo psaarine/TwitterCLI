836301fbd98e04d21c491ed63686fcd5b9cac733
Pyry Saarinen <pyry.saarinen@digia.com>
2022-04-04 15:34:41 +0300

    query string parsing

--- src/login.rs
+++ src/login.rs
@@ -1,4 +1,9 @@
-fn login() {
+use std::net::
+{ TcpListener, TcpStream };
+use std::io::prelude::*;
+use std::str;
+
+pub fn login() {
 
     /*
 
@@ -7,6 +12,10 @@ fn login() {
     Parses Id from the identification.
 
      */
+
+     create_tcp_listener();
+     //open_browser(&generate_login_url());
+
 }
 
 fn open_browser(url: &str) {
@@ -25,4 +34,62 @@ fn create_tcp_listener(){
     Creates a tcplistener to a specified port to capture redirectionUrl:s
 
      */
+
+     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+     for stream in listener.incoming() {
+         let stream = stream.unwrap();
+
+         handle_stream(stream);
+     }
+}
+
+fn generate_login_url() -> String {
+
+    return String::from("asd");
+}
+
+fn handle_stream(mut stream: TcpStream) {
+
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let id = parse_received_url(str::from_utf8(&buffer).unwrap().to_owned());
+    println!("{}", id);
+}
+
+fn parse_received_url(data: String) -> String {
+
+    let mut parts = data.split_whitespace();
+    
+    let part1 = parts.next().unwrap();
+    let part2 = parts.next().unwrap();
+    let part3 = parts.next().unwrap();
+
+    return format!("Part 1: {}, part 2: {}, part 3: {}", part1, part2, part3);
+}
+
+fn parse_twitter_return_url(url: &str) {
+
+    let query_part = url.split("?").last().unwrap_or("Query string was maliformed");
+    
+}
+
+struct AuthResult {
+
+    oauth_token: String,
+    oauth_verifier: String
+
+}
+
+#[cfg(test)]
+mod tests {
+    
+    use super::*;
+    #[test]
+    pub fn test_return_string_parser() {
+
+        // tests that method can parse valid information from return string
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
 mod login;
+mod utils;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+
+use login::login;
 
 fn main() {
 
 }
-
-pub struct AuthenticationResult {
-
-}
-
-fn basic_program_flow(handler: fn()){
-
-    /*
-
-    Basic program flow goes as follows:
-
-    Tries to execute handler. If receives unauthenticated, will first check expiration token validity.
-    If unable to get a valid token
-
-     */
-
-
-
-}
-fn authenticate() -> AuthenticationResult {
-
-    /*
-
-    Initializes an authentication flow. Opens a browser in the twitter login page and once user has signed in will capture redirection
-    and parse tokens from the
-     */
-    return AuthenticationResult {}
-}
-
